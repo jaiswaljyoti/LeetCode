28659e7bb31d3d6299d29ff8b47db91e3a29f0c6
jaiswaljyoti <jaiswaljyoti2000@gmail.com>
2022-03-23 20:30:34 +0530

    https://leetcode.com/problems/broken-calculator/submissions/

---
+++ March challenges 2022/Broken calculator.py
@@ -0,0 +1,16 @@
+class Solution:
+    def brokenCalc(self, startValue: int, target: int) -> int:
+        if startValue>=target:
+            return startValue-target
+        
+        else:
+            steps = 0
+            while target > startValue:
+                if target % 2 != 0:
+                    target += 1
+                else:
+                    target = target//2
+                steps += 1
+            steps += (startValue - target)
+            
+            return steps
